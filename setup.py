a4894fb02f7faf50aabbb339396b8b464211e15e
Pravvenakaliyaperumal <praveenakaliyaperumal@gmail.com>
2025-12-04 15:14:06 -0500

    requirements.txtupdated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='retrival_system',
+    version='0.0.0',
+    author='praveena',
+    author_email='addedmemory@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
